03823ac3790108a38997b0180d7cb06a0e1711a0
OWID <owid@ourworldindata.org>
2018-01-10 16:33:12 -0500

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,7 +5,7 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = '/grapher/assets/charts.bundle.42207f993662516fd484.css';
+        link.href = '/grapher/assets/charts.bundle.c271812b0d99e0c93417.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
@@ -28,6 +28,6 @@
             if (hasPolyfill)
                 window.Grapher.embedAll();
         }
-        script.src = '/grapher/assets/charts.bundle.42207f993662516fd484.js';
+        script.src = '/grapher/assets/charts.bundle.c271812b0d99e0c93417.js';
         document.head.appendChild(script);
     
