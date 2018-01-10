9a87516ff9e44a04e4c7c423fb33e5cf338fd879
OWID <owid@ourworldindata.org>
2018-01-10 18:14:04 -0500

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,7 +5,7 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = '/grapher/assets/charts.bundle.4655b6e01b60e75109bb.css';
+        link.href = '/grapher/assets/charts.bundle.f235081623f803810072.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
@@ -28,6 +28,6 @@
             if (hasPolyfill)
                 window.Grapher.embedAll();
         }
-        script.src = '/grapher/assets/charts.bundle.4655b6e01b60e75109bb.js';
+        script.src = '/grapher/assets/charts.bundle.f235081623f803810072.js';
         document.head.appendChild(script);
     
