0dc5300713576a1b0739904750eec2bfdb1c9b28
OWID <owid@ourworldindata.org>
2018-03-20 06:13:56 +0000

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,29 +5,34 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = 'https://test-grapher.netlify.com/grapher/assets/charts.css?v=charts.bundle.7e2472ea318e518f9069.css';
+        link.href = 'https://test-grapher.netlify.com/grapher/assets/commons.css?v=commons.bundle.90c169ad51ecf04c4835.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
         var hasGrapher = false;
 
+        var loadedScripts = 0;
+        function checkReady() {
+            if (loadedScripts == 3) {
+                window.Grapher.embedAll();
+            }
+        }
+
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.onload = function() {
-            hasPolyfill = true;
-            if (hasGrapher)
-                window.Grapher.embedAll();
-        }
+        script.onload = checkReady;
         script.src = "https://cdn.polyfill.io/v2/polyfill.min.js?features=es6,fetch"
         document.head.appendChild(script);
 
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.onload = function() {
-            hasGrapher = true;
-            if (hasPolyfill)
-                window.Grapher.embedAll();
-        }
-        script.src = 'https://test-grapher.netlify.com/grapher/assets/charts.js?v=charts.bundle.7e2472ea318e518f9069.js';
+        script.onload = checkReady;
+        script.src = 'https://test-grapher.netlify.com/grapher/assets/commons.js';
+        document.head.appendChild(script);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = 'https://test-grapher.netlify.com/grapher/assets/charts.js?v=charts.bundle.dcf65b68eddaba4c6a58.js';
         document.head.appendChild(script);
     
