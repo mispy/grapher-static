11772235aaf73460ddb691977d49cfcb3a33d177
OWID <owid@ourworldindata.org>
2018-03-27 05:53:05 +0000

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,7 +5,7 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = 'https://test-grapher.netlify.com/grapher/assets/commons.css?v=commons.bundle.34ce403b25efd4452793.css';
+        link.href = 'https://test-grapher.netlify.com/grapher/assets/commons.css?v=commons.bundle.a70076a9912336cf4446.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
@@ -28,7 +28,7 @@
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.onload = checkReady;
-        script.src = 'https://test-grapher.netlify.com/grapher/assets/commons.js?v=commons.bundle.34ce403b25efd4452793.js';
+        script.src = 'https://test-grapher.netlify.com/grapher/assets/commons.js?v=commons.bundle.a70076a9912336cf4446.js';
         document.head.appendChild(script);
 
         var script = document.createElement('script');
