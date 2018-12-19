4d68dce4dfd0afd65d833e3c9e13e3c6b32ba37b
OWID <owid@ourworldindata.org>
2018-12-19 13:26:26 +0000

    Automated update

--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -5,7 +5,7 @@
         var link = document.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        link.href = 'https://mispytest-grapher.netlify.com/grapher/assets/commons.bundle.2e22f5e69cab1ba9e613.css';
+        link.href = 'https://mispytest-grapher.netlify.com/grapher/assets/commons.bundle.0e1ca6447fea47208fff.css';
         document.head.appendChild(link);
 
         var hasPolyfill = false;
@@ -28,12 +28,12 @@
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.onload = checkReady;
-        script.src = 'https://mispytest-grapher.netlify.com/grapher/assets/commons.bundle.2e22f5e69cab1ba9e613.js';
+        script.src = 'https://mispytest-grapher.netlify.com/grapher/assets/commons.bundle.0e1ca6447fea47208fff.js';
         document.head.appendChild(script);
 
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.onload = checkReady;
-        script.src = 'https://mispytest-grapher.netlify.com/grapher/assets/charts.bundle.2e22f5e69cab1ba9e613.js';
+        script.src = 'https://mispytest-grapher.netlify.com/grapher/assets/charts.bundle.0e1ca6447fea47208fff.js';
         document.head.appendChild(script);
     
